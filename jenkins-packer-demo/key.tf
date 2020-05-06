2af500c7b2fb205f664d2a1e653f2c2a816b033c
skgeorge <skgeorge@hotmail.com>
2020-05-06 15:21:09 +0530

    Update key.tf

--- jenkins-packer-demo/key.tf
+++ jenkins-packer-demo/key.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "mykeypair" {
-  key_name   = "mykeypair"
+resource "aws_key_pair" "mykeypair1" {
+  key_name   = "mykeypair1"
   public_key = fileexists(var.PATH_TO_PUBLIC_KEY) ? file(var.PATH_TO_PUBLIC_KEY) : var.DUMMY_SSH_PUB_KEY
   lifecycle {
     ignore_changes = [public_key]
